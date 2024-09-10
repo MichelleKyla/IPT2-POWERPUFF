f70044270733637b9931ec3b284f58d8d628d99c
MichelleKyla <michelletamayo931@gmail.com>
2024-09-10 11:44:40 +0800

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -1,5 +1,24 @@
-class Addition {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
+import java.util.Scanner;
+public class Addition{ 
+
+    public static void main(String args[]){  
+     
+     Scanner scan = new Scanner(System.in);
+
+     System.out.print("Enter the first Number: ");
+     double num1 = scan.nextDouble();
+
+     System.out.print("Enter the second Number: ");
+     double num2 = scan.nextDouble();
+
+     double result = num1 + num2;
+
+     System.out.println("Result: "+ result);
+    
+    scan.close();
+
+
+      
+    }  
+}  
+
